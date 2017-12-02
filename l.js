a80251e4f8c2794714e53cb869101f18fa0a0a57
aliveender <thealiveender@gmail.com>
2017-12-02 12:20:56 +0000

    Update l.js

---
+++ l.js
@@ -0,0 +1,12 @@
+const magik = magikcraft.io;
+
+function l() {
+    const there = magik.aspecto();
+    magik.shakti(there);
+    magik.shakti(there);
+    magik.shakti(there);
+    magik.shakti(there);
+    magik.shakti(there);
+    magik.shakti(there);  
+
+}
