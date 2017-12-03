bd562598632c62ec5fbb6025bceca4ca31f6d22d
aliveender <thealiveender@gmail.com>
2017-12-03 01:48:29 +0000

    Update l.js

--- l.js
+++ l.js
@@ -2,17 +2,17 @@ const magik = magikcraft.io;
 
 function l() {
     const there = magik.aspecto();
-    magik.shakti(player);
-    magik.shakti(player);
-    magik.shakti(player);
-    magik.shakti(player);
-    magik.shakti(player);
-    magik.shakti(player);  
-    magik.shakti(player);
-    magik.shakti(player);
-    magik.shakti(player);
-    magik.shakti(player);
-    magik.shakti(player);
-    magik.shakti(player);
+    magik.shakti(there);
+    magik.shakti(there);   
+    magik.shakti(there);    
+    magik.shakti(there);    
+    magik.shakti(there);    
+    magik.shakti(there);    
+    magik.shakti(there);    
+    magik.shakti(there);    
+    magik.shakti(there);    
+    magik.shakti(there);
+    magik.shakti(there);
+    magik.shakti(there);
     
 }
